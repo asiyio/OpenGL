211f8ddc386263a8f281685b52fc191111090ba0
asiyio <230604910@qq.com>
2024-08-04 23:57:11 +0800

    add glm & imgui

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -14,7 +14,7 @@
 #include "program.h"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb/stb_image.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
