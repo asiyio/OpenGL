63953e275a53b49b9bda104ac51ff19df58690ab
asiyio <230604910@qq.com>
2024-08-09 22:34:46 +0800

    switch to macOS app

---
+++ LearnOpenGL/ResourcePath.h
@@ -0,0 +1,15 @@
+//
+//  ResourcePath.h
+//  LearnOpenGL
+//
+//  Created by asi on 2024/8/9.
+//
+
+#ifndef ResourcePath_h
+#define ResourcePath_h
+
+#include <string>
+
+std::string ResourcePathWithFile(const std::string& file);
+
+#endif /* ResourcePath_h */
